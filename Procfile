53b8561831443ff7f1d51a0dfff4779fb5592f2f
felexkemboi <felokemboi10@gmail.com>
2018-04-29 19:12:47 +0300

    all

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn g4us.wsgi
--- mentorship/settings.py
+++ mentorship/settings.py
@@ -121,46 +121,17 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-
-#MEDIA_ROOT = '/media/'
-
-#STATICFILES_DIRS = (
- #    os.path.join(BASE_DIR, 'static'),
-#)
-
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-#MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
-#STATIC_URL = '/static/'
 
-#document_root = os.path.join(BASE_DIR, 'media')
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'index'
 
-django_heroku.settings(locals())
 
