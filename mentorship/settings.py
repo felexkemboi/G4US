d29046a2be267107f3e58b51656d93fc6d137708
felexkemboi <felokemboi10@gmail.com>
2018-04-29 16:19:47 +0300

    first commit

--- mentorship/settings.py
+++ mentorship/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
           default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
       )
   }
+  
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
